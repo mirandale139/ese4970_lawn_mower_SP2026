4562d71ee93ed0a2c4ff8e58ec3b0dc24db1ddb7
mirandale139 <143383608+mirandale139@users.noreply.github.com>
2026-02-18 14:52:44 -0600

    Update camera.py

--- Components/camera.py
+++ Components/camera.py
@@ -1,7 +1,11 @@
 import subprocess
+import time
 
 # File path to save image
-image_path = "/home/pi/Desktop/test2seconds.jpg"
+save_dir = "/home/gle/ESE4970/grass_output"
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+image_path = os.path.join(save_dir, f"test_cam_{timestamp}.jpg")
+
 
 # Capture image with 2-second preview
 subprocess.run(["rpicam-still", "-t", "2000", "-o", image_path])
